e4991fbfc5314b47bb90d9c42a2a95b080601935
lucifer-kj <askarif0@gmail.com>
2025-09-10 22:22:32 +0530

    deploy

---
+++ pages/framer-embed.js
@@ -0,0 +1,14 @@
+const FramerIframePage = () => {
+  return (
+    <div>
+      <iframe 
+        src="https://prasoutech.framer.website/" 
+        width="100%" 
+        height="600px" 
+        allowFullScreen 
+      />
+    </div>
+  );
+};
+
+export default FramerIframePage;
